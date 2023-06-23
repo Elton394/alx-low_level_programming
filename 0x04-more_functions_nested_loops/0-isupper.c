3a60d22384cfa7109a58ed4374f46f0f6a7be5c6
Elton Asamoah-Mantey <eltonasamoah@gmail.com>
2023-06-22 18:14:52 -0700

    Check  for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase
+ * @c : the character to be checked
+ * Return: 1 for uppercase character, 0 for anything else
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
